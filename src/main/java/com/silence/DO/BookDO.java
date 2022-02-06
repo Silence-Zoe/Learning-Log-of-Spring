f6f1c05602ea58d3f30c14a86261dfd2d7cf7b90
Silence <silence1874@foxmail.com>
2022-02-06 13:17:39 +0800

    Create BookDO

---
+++ src/main/java/com/silence/DO/BookDO.java
@@ -0,0 +1,11 @@
+package com.silence.DO;
+
+import lombok.Data;
+
+@Data
+public class BookDO {
+    private Integer id;
+    private String type;
+    private String name;
+    private String description;
+}

c8326b5980baf0619d649f0b7efbdc18c5e13076
Silence <silence1874@foxmail.com>
2022-03-18 12:46:55 +0800

    Initialize

---
+++ src/main/java/com/silence/NowcoderApplication.java
@@ -0,0 +1,11 @@
+package com.silence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NowcoderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NowcoderApplication.class, args);
+    }
+}
---
+++ src/test/java/com/silence/NowcoderApplicationTest.java
@@ -0,0 +1,12 @@
+package com.silence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class NowcoderApplicationTest {
+    @Test
+    void test() {
+        System.out.println("hello test");
+    }
+}

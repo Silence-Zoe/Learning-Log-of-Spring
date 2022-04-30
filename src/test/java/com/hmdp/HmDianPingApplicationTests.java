8443889910237bb2cf4c34c1ed2c64d8b380b948
Silence <silence1874@foxmail.com>
2022-04-30 19:28:07 +0800

    Initialize

---
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -0,0 +1,14 @@
+package com.hmdp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmDianPingApplicationTests {
+
+    @Test
+    public void testConnection() {
+
+    }
+
+}

d98f31308fe670993c5aeb78ff9a5f0e9b4309f9
Silence <silence1874@foxmail.com>
2022-05-01 16:30:08 +0800

    Login

--- src/main/java/com/hmdp/service/impl/UserServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.hmdp.service.impl;
 
 import cn.hutool.core.util.RandomUtil;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 import com.hmdp.mapper.UserMapper;
@@ -11,6 +12,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
 
 /**
  * <p>
@@ -41,4 +44,46 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IU
         log.debug("已发送短信，验证码为：{}", code);
         return Result.ok();
     }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+
+        // 校验手机号
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            return Result.fail("请输入正确的手机号！");
+        }
+
+        // 校验验证码
+        Object cacheCode = session.getAttribute("code");
+        String code = loginForm.getCode();
+        if (cacheCode == null || !cacheCode.toString().equals(code)) {
+            return Result.fail("验证码错误！");
+        }
+
+        // 根据手机号查询用户
+        User user = query().eq("phone", phone).one();
+
+        // 判断用户是否存在
+        if (user == null) {
+            // 不存在则创建
+            user = createUserWithPhone(phone);
+        }
+
+        // 保存用户信息到session中
+        session.setAttribute("user", user);
+        return Result.ok();
+    }
+
+    private User createUserWithPhone(String phone) {
+        // 创建用户
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomNumbers(10));
+
+        // 保存用户
+        save(user);
+        return user;
+    }
+
 }
